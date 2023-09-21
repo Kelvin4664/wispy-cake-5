22f3306a99d5423f3fa64ff964329d13f2d0053a
kelvin4664 <kelvinadigwu@yahoo.com>
2023-09-21 20:47:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wispy-cake-5",
+  url: "https://wispy-cake-5.botics.co",
+  api: "https://wispy-cake-5.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
